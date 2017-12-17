bee8bf11fb79d56124e599325d5e2ec949b40e0e
Fintan Ward <fintan.ward@teamaol.com>
2017-12-17 00:41:10 +0000

    master : First draft with all features implemented.

--- src/main/java/com/workday/DataScienceExcerise.java
+++ src/main/java/com/workday/DataScienceExcerise.java
@@ -1,7 +1,10 @@
 package com.workday;
 
+import com.sun.tools.corba.se.idl.constExpr.Not;
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -21,46 +24,94 @@ public class DataScienceExcerise
         dse.displayMenu();
     }
 
-    public void getNationData() {
+    private void getNationData()
+    {
         ParseNations1File nations1Parser = new ParseNations1File();
         nationalData = nations1Parser.getData();
-        //System.out.println("DataScienceExcerise : getNationDataSet : size  : " + nationalData.size());
 
         ParseNations2File nations2Parser = new ParseNations2File();
         nations2Parser.getData(nationalData);
-        //System.out.println("DataScienceExcerise : getNationDataSet : size : " + nationalData.size());
     }
 
-    public void displayMenu()
+    private void displayMenu()
     {
-        int choice = 0;
+        int choice;
+        boolean poppingMenu;
         boolean quit = false;
         while (!quit) {
+            poppingMenu = false;
             printInstructions();
             System.out.println("Enter your choice : ");
             choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch(choice) {
-                case 0 :
+            switch (choice) {
+                case 0:
                     printInstructions();
                     break;
-                case 1 :
-                    printData();
+                case 1:
+                    Collections.sort(nationalData);
+                    printData(nationalData);
                     break;
-                case 2 :
-                    //contacts.AddContact();
+                case 2:
+                    poppingMenu = displayOutliersMenu();
                     break;
-                case 3 :
-                    //contacts.UpdateContact();
+                case 3:
+                    findLowerIncomes();
                     break;
-                case 4 :
-                    //contacts.RemoveContact();
+                case 4:
+                    findingMatchingRegion();
                     break;
-                case 5 :
-                    //contacts.SearchContacts();
+                case 5:
+                    quit = true;
                     break;
-                case 6 :
+            }
+
+            if (!quit) {
+                if (!poppingMenu) {
+                    System.out.println("Press Enter To Continue");
+                    scanner.nextLine();
+                }
+            }
+        }
+    }
+
+    private void printInstructions()
+    {
+        System.out.println("\nMain Menu\n=========");
+        System.out.println("Press ");
+        System.out.println("\t0 - To print choice options");
+        System.out.println("\t1 - Print Data");
+        System.out.println("\t2 - Display Outliers");
+        System.out.println("\t3 - Display regions with systematically lower income");
+        System.out.println("\t4 - Enter new national data and display relevant country");
+        System.out.println("\t5 - To quit the application");
+    }
+
+    private boolean displayOutliersMenu()
+    {
+        int choice;
+        boolean quit = false;
+        while (!quit) {
+            printOutliersInstruction();
+            System.out.println("Enter your choice : ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 0:
+                    printOutliersInstruction();
+                    break;
+                case 1:
+                    displayPopulationOutlier();
+                    break;
+                case 2:
+                    displayGDPOutlier();
+                    break;
+                case 3:
+                    displayLifeExpectencyOutlier();
+                    break;
+                case 4:
                     quit = true;
                     break;
             }
@@ -70,22 +121,236 @@ public class DataScienceExcerise
                 scanner.nextLine();
             }
         }
+        return true;
     }
 
-    public void printInstructions()
+    private void printOutliersInstruction()
     {
-        System.out.println("\nPress ");
+        System.out.println("\nOutliners Menu\n==============");
+        System.out.println("Press ");
         System.out.println("\t0 - To print choice options");
-        System.out.println("\t1 - Print Data");
-        System.out.println("\t2 - Display Outliners");
-        System.out.println("\t3 - Display Outlines ignoring population");
-        System.out.println("\t4 - Display regions wiht systematically lower income");
-        System.out.println("\t5 - Enter new national data and display relevant country");
-        System.out.println("\t6 - To quit the application");
+        System.out.println("\t1 - Display Population Outliers");
+        System.out.println("\t2 - Display GDP Outliers");
+        System.out.println("\t3 - Display Life Expectency Outliers");
+        System.out.println("\t4 - Go back to main menu");
     }
 
 
-    private void printData() {
+    private void displayLifeExpectencyOutlier()
+    {
+        Collections.sort(nationalData, NationsRecord.getLifeExpectencyComparator());
+        float size = nationalData.size();
+        float middle = size / 2;
+        float meanPosition = (size / 2) / 2;
+        float lowerMean = (0 + meanPosition);
+        float higherMean = (middle + meanPosition);
+
+        Double Q1 = nationalData.get((int) lowerMean - 1).getLife_expect() +
+                nationalData.get((int) lowerMean + 1).getLife_expect() / 2;
+        Double Q3 = nationalData.get((int) higherMean - 1).getLife_expect() +
+                nationalData.get((int) higherMean + 1).getLife_expect() / 2;
+
+        Double innerQuartileRange = Q3 - Q1;
+        Double HighOutliner = Q3 + (1.5 * innerQuartileRange);
+        Double LowOutliner = Q1 - (1.5 * innerQuartileRange);
+
+        ArrayList<NationsRecord> LowOutliers = new ArrayList<>();
+        ArrayList<NationsRecord> HighOutliers = new ArrayList<>();
+        for (NationsRecord nr : nationalData) {
+            if (nr.getLife_expect() < LowOutliner)
+                LowOutliers.add(nr);
+            if (nr.getLife_expect() > HighOutliner)
+                HighOutliers.add(nr);
+        }
+        System.out.println("HighOutliers  : (" + HighOutliner + ") : " + HighOutliers.size());
+        if (HighOutliers.size() > 0) printData(HighOutliers);
+        System.out.println("LowOutliers  : (" + LowOutliner + ") : " + LowOutliers.size());
+        if (LowOutliers.size() > 0) printData(LowOutliers);
+    }
+
+    private void displayGDPOutlier()
+    {
+        Collections.sort(nationalData, NationsRecord.getGDPComparator());
+        float size = nationalData.size();
+        float middle = size / 2;
+        float meanPosition = (size / 2) / 2;
+        float lowerMean = (0 + meanPosition);
+        float higherMean = (middle + meanPosition);
+
+        Double Q1 = nationalData.get((int) lowerMean - 1).getGdp_percap() +
+                nationalData.get((int) lowerMean + 1).getGdp_percap() / 2;
+        Double Q3 = nationalData.get((int) higherMean - 1).getGdp_percap() +
+                nationalData.get((int) higherMean + 1).getGdp_percap() / 2;
+
+        Double innerQuartileRange = Q3 - Q1;
+        Double HighOutliner = Q3 + (1.5 * innerQuartileRange);
+        Double LowOutliner = Q1 - (1.5 * innerQuartileRange);
+
+        ArrayList<NationsRecord> LowOutliers = new ArrayList<>();
+        ArrayList<NationsRecord> HighOutliers = new ArrayList<>();
+        for (NationsRecord nr : nationalData) {
+            if (nr.getGdp_percap() < LowOutliner)
+                LowOutliers.add(nr);
+            if (nr.getGdp_percap() > HighOutliner)
+                HighOutliers.add(nr);
+        }
+        System.out.println("HighOutliers  : (" + HighOutliner + ") : " + HighOutliers.size());
+        if (HighOutliers.size() > 0) printData(HighOutliers);
+        System.out.println("LowOutliers  : (" + LowOutliner + ") : " + LowOutliers.size());
+        if (LowOutliers.size() > 0) printData(LowOutliers);
+    }
+
+    private void displayPopulationOutlier()
+    {
+        Collections.sort(nationalData, NationsRecord.getPopulationComparator());
+        float size = nationalData.size();
+        float middle = size / 2;
+        float meanPosition = (size / 2) / 2;
+        float lowerMean = (0 + meanPosition);
+        float higherMean = (middle + meanPosition);
+
+        double Q1 = nationalData.get((int) lowerMean - 1).getPopulation() +
+                nationalData.get((int) lowerMean + 1).getPopulation() / 2;
+        double Q3 = nationalData.get((int) higherMean - 1).getPopulation() +
+                nationalData.get((int) higherMean + 1).getPopulation() / 2;
+
+        double innerQuartileRange = Q3 - Q1;
+        double HighOutliner = Q3 + (1.5 * innerQuartileRange);
+        double LowOutliner = Q1 - (1.5 * innerQuartileRange);
+
+        ArrayList<NationsRecord> LowOutliers = new ArrayList<>();
+        ArrayList<NationsRecord> HighOutliers = new ArrayList<>();
+        for (NationsRecord nr : nationalData) {
+            if (nr.getPopulation() < LowOutliner)
+                LowOutliers.add(nr);
+            if (nr.getPopulation() > HighOutliner)
+                HighOutliers.add(nr);
+        }
+        System.out.println("HighOutliers  : (" + HighOutliner + ") : " + HighOutliers.size());
+        if (HighOutliers.size() > 0) printData(HighOutliers);
+        System.out.println("LowOutliers  : (" + LowOutliner + ") : " + LowOutliers.size());
+        if (LowOutliers.size() > 0) printData(LowOutliers);
+    }
+
+    private void findLowerIncomes()
+    {
+        Map<String, Integer> incomeBrackets = new HashMap<>();
+        incomeBrackets.put("Not classified", 0);
+        incomeBrackets.put("High income", 40);
+        incomeBrackets.put("Upper middle income", 30);
+        incomeBrackets.put("Lower middle income", 20);
+        incomeBrackets.put("Low income", 10);
+
+        Map<String, Double> regions = new HashMap<>();
+        Map<String, Double> regionsCount = new HashMap<>();
+        for (NationsRecord nr : nationalData) {
+            String region = nr.getRegion();
+            String nrIncomeBracket = nr.getIncome();
+            double allIncome = 0;
+            if (regions.containsKey(region)) {
+                allIncome = regions.get(region);
+            }
+            allIncome += incomeBrackets.get(nrIncomeBracket);
+            regions.put(region, allIncome);
+
+            double regionCount = 0;
+            if (regionsCount.containsKey(region)) {
+                regionCount = regionsCount.get(region);
+            }
+            regionsCount.put(region, ++regionCount);
+        }
+
+        String regionName = "";
+        double lowestIncome = 9999;
+        for (String region : regions.keySet()) {
+            double avgIncome = regions.get(region) / regionsCount.get(region);
+            if (lowestIncome > avgIncome) {
+                lowestIncome = avgIncome;
+                regionName = region;
+            }
+        }
+        System.out.println("The region with systematically lower income is : [" + regionName + "]");
+    }
+
+    private void findingMatchingRegion()
+    {
+        NationsRecord unknownNR = enterNationalData();
+        NationsRecord selectedNR = findExactMatch(unknownNR);
+        String region = "No Match Found";
+        if (selectedNR != null)
+            region = selectedNR.getRegion();
+        System.out.println("Region Detected : " + region);
+        scanner.nextLine();
+    }
+
+    private NationsRecord enterNationalData()
+    {
+        System.out.print("Enter country data now.");
+
+        System.out.print("\nEnter Life Expectency : ");
+        String lifeExpectency = scanner.next();
+
+        System.out.print("\nEnter Population : ");
+        String population = scanner.next();
+
+        System.out.print("\nEnter Birth Rate : ");
+        String birthRate = scanner.next();
+
+        System.out.print("\nEnter Neo Natel Mortality Rate : ");
+        String neonat_mortal_rate = scanner.next();
+
+        System.out.print("\nEnter GDP Per Capita : ");
+        String gdp_percap = scanner.next();
+
+        Map<String, String> newRecord = new HashMap<>();
+        newRecord.put("iso2c", "XX");
+        newRecord.put("iso3c", "XXX");
+        newRecord.put("country", "XXXX");
+        newRecord.put("year", "2014");
+        newRecord.put("life_expect", lifeExpectency);
+        newRecord.put("population", population);
+        newRecord.put("birth_rate", birthRate);
+        newRecord.put("neonat_mortal_rate", neonat_mortal_rate);
+        newRecord.put("region", "XXXX");
+        newRecord.put("income", "0.0");
+        newRecord.put("gdp_percap", gdp_percap);
+        return new NationsRecord(newRecord);
+    }
+
+    private NationsRecord findExactMatch(NationsRecord unknownNR)
+    {
+        Collections.sort(nationalData, NationsRecord.getPopulationComparator());
+        ArrayList<NationsRecord> nearMatchingNR = new ArrayList<>();
+
+        // Check for near matches
+        for (NationsRecord nr : nationalData) {
+            Double unKnownLE = unknownNR.getLife_expect();
+            Double nrLE = nr.getLife_expect();
+            if ((unKnownLE + 1 > nrLE) && (unKnownLE - 1 < nrLE))
+                nearMatchingNR.add(nr);
+        }
+
+        // No matches found
+        if (nearMatchingNR.size() == 0)
+            return null;
+
+        // Narrow to closest match
+        Double curDiff;
+        Double matchedDiff = 100.0;
+        NationsRecord finalMatch = null;
+        for (NationsRecord nr : nearMatchingNR) {
+            curDiff = Math.abs(unknownNR.getLife_expect() - nr.getLife_expect());
+            if (matchedDiff > curDiff) {
+                matchedDiff = curDiff;
+                finalMatch = nr;
+            }
+        }
+        System.out.println();
+        return finalMatch;
+    }
+
+    private void printData(ArrayList<NationsRecord> nationalData)
+    {
         List<String> allHeaders = new ArrayList<>();
         allHeaders.add("iso2c");
         allHeaders.add("iso3c");
@@ -98,9 +363,8 @@ public class DataScienceExcerise
         allHeaders.add("region");
         allHeaders.add("income");
         allHeaders.add("gdp_percap");
-        System.out.println("allHeaders :" + allHeaders);
 
-        System.out.print("Headers   : ");
+        System.out.print("Headers    : ");
         int charTab = 10;
         for (String field : allHeaders) {
             switch (field) {
@@ -138,16 +402,16 @@ public class DataScienceExcerise
                     charTab = 16;
                     break;
             }
-            String text = "[" + field + "], ";
+            StringBuilder text = new StringBuilder();
+            text.append("[").append(field).append("], ");
             for (int j = field.length(); j < charTab; j++) {
-                text += " ";
+                text.append(" ");
             }
-            System.out.print(text);
+            System.out.print(text.toString());
         }
 
-        int i=0;
+        int i = 0;
         charTab = 10;
-        Collections.sort(nationalData);
         for (NationsRecord record : nationalData) {
             System.out.print(String.format("\nRecord %03d : ", ++i));
             for (String field : allHeaders) {
@@ -167,7 +431,7 @@ public class DataScienceExcerise
                         charTab = 30;
                         break;
                     case "year":
-                        fieldValue = new Integer(record.getYear()).toString();
+                        fieldValue = Integer.toString(record.getYear());
                         charTab = 8;
                         break;
                     case "life_expect":
@@ -199,11 +463,12 @@ public class DataScienceExcerise
                         charTab = 16;
                         break;
                 }
-                String text = "[" + fieldValue + "], ";
+                StringBuilder text = new StringBuilder();
+                text.append("[").append(fieldValue).append("], ");
                 for (int j = fieldValue.length(); j < charTab; j++) {
-                    text += " ";
+                    text.append(" ");
                 }
-                System.out.print(text);
+                System.out.print(text.toString());
             }
         }
         System.out.println("");
--- src/main/java/com/workday/NationsRecord.java
+++ src/main/java/com/workday/NationsRecord.java
@@ -1,5 +1,7 @@
 package com.workday;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 
 /**
@@ -19,8 +21,35 @@ public class NationsRecord implements Comparable<NationsRecord>
     private String income = "";
     private Double gdp_percap = 0.0;
 
-     public NationsRecord(Map<String, String> record)
-     {
+    static Comparator<NationsRecord> getPopulationComparator() {
+        return new Comparator<NationsRecord>() {
+            public int compare(NationsRecord nr1,NationsRecord nr2)
+            {
+                return nr1.getPopulation().compareTo(nr2.getPopulation());
+            }
+        };
+    }
+
+    static Comparator<NationsRecord> getGDPComparator() {
+        return new Comparator<NationsRecord>() {
+            public int compare(NationsRecord nr1,NationsRecord nr2)
+            {
+                return nr1.getGdp_percap().compareTo(nr2.getGdp_percap());
+            }
+        };
+    }
+
+    static Comparator<NationsRecord> getLifeExpectencyComparator() {
+        return new Comparator<NationsRecord>() {
+            public int compare(NationsRecord nr1,NationsRecord nr2)
+            {
+                return nr1.getLife_expect().compareTo(nr2.getLife_expect());
+            }
+        };
+    }
+
+    public NationsRecord(Map<String, String> record)
+    {
         this.iso2c = record.get("iso2c");
         this.iso3c = record.get("iso3c");
         this.country = record.get("country");
@@ -30,25 +59,25 @@ public class NationsRecord implements Comparable<NationsRecord>
         if (life_expect != null && life_expect.length() > 0)
             this.life_expect = new Double(record.get("life_expect"));
         else
-            this.life_expect =0.0;
+            this.life_expect = 0.0;
 
         String population = record.get("population");
         if (population != null && population.length() > 0)
             this.population = new Double(record.get("population"));
         else
-            this.population =0.0;
+            this.population = 0.0;
 
         String birth_rate = record.get("birth_rate");
         if (birth_rate != null && birth_rate.length() > 0)
             this.birth_rate = new Double(record.get("birth_rate"));
         else
-            this.birth_rate =0.0;
+            this.birth_rate = 0.0;
 
-         String neonat_mortal_rate = record.get("neonat_mortal_rate");
-         if (neonat_mortal_rate != null && neonat_mortal_rate.length() > 0)
-             this.neonat_mortal_rate = new Double(record.get("neonat_mortal_rate"));
-         else
-             this.neonat_mortal_rate =0.0;
+        String neonat_mortal_rate = record.get("neonat_mortal_rate");
+        if (neonat_mortal_rate != null && neonat_mortal_rate.length() > 0)
+            this.neonat_mortal_rate = new Double(record.get("neonat_mortal_rate"));
+        else
+            this.neonat_mortal_rate = 0.0;
 
         this.region = record.get("region");
         this.income = record.get("income");
@@ -67,7 +96,7 @@ public class NationsRecord implements Comparable<NationsRecord>
 
     public void setGdp_percap(String gdp_percap)
     {
-        this.gdp_percap = (gdp_percap != null && gdp_percap.length()>0)?new Double(gdp_percap):0.0;
+        this.gdp_percap = (gdp_percap != null && gdp_percap.length() > 0) ? new Double(gdp_percap) : 0.0;
     }
 
     public Double getGdp_percap()
